3ea776abd4f25a91122d3e4021f6946a927e51cb
hedmon84 <hedmonlopes@gmail.com>
2018-03-31 16:36:56 -0600

    Succes

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 
 using namespace std;
 int main(){
+
+
     Utilidades Create;
-    nodo *list = nullptr;
-    nodo *list2 = Create.CrearLista(10);
-    Create.InsertarFinal(list,5);
-    Create.InsertarFinal(list, 9);
+    nodo *list1 = nullptr;
+
+    //cout<<"Lista 1 agregar manual al final de la lista \n";
+
+    Create.InsertarFinal(list1,5);
+    Create.InsertarFinal(list1, 9);
     //Create.ImprimirLista(list);
+
+    cout<<"Lista 2 Random \n ";
+    nodo *list2 = Create.CrearLista(10);
     Create.ImprimirLista(list2);
     Create.OrdenarLista(list2);
     Create.ImprimirLista(list2);
