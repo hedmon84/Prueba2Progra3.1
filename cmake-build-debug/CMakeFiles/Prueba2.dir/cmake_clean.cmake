2638915b09fb96069c3a2f3ea95518851c58ba81
hedmon84 <hedmonlopes@gmail.com>
2018-03-31 15:08:08 -0600

    add
    succes

---
+++ cmake-build-debug/CMakeFiles/Prueba2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prueba2.dir/main.cpp.o"
+  "CMakeFiles/Prueba2.dir/Utilidades.cpp.o"
+  "Prueba2.pdb"
+  "Prueba2.exe"
+  "libPrueba2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prueba2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
